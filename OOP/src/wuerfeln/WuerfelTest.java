a89cab3e69c91a69b09f05add1a838b77d930181
Philip <Philip@Philip-PC>
2016-04-13 16:56:12 +0200

    fixed scanner

--- OOP/src/wuerfeln/WuerfelTest.java
+++ OOP/src/wuerfeln/WuerfelTest.java
@@ -12,29 +12,31 @@ public class WuerfelTest {
 	public static void main(String[] args){
 		boolean exec = true;
 		int min = 1;
+		Scanner sc = new Scanner(System.in);
 		while(exec){
 			System.out.println("Legen sie die maximale Augenzahl des Würfels fest: ");
-			Scanner sc = new Scanner(System.in);
 			int max = sc.nextInt();
 			System.out.println("Wie oft soll gewuerfelt werden?");
 			int anzahlVersuche = sc.nextInt();
 			Wuerfel dice = new Wuerfel(min, max);
 			Haeufigkeiten stats = new Haeufigkeiten(max, anzahlVersuche);
-			Fehler fehl = new Fehler(stats);
+			//Fehler fehl = new Fehler(stats);
 			
 			for(int i = 0; i < anzahlVersuche; i++){
 				stats.addAugenzahl(dice.wuerfeln());
 			}
 			
-			fehl.Fehlerberechnung();
+			//fehl.Fehlerberechnung();
 			System.out.println(stats.toString());
-			System.out.println(fehl.toString());
+			//System.out.println(fehl.toString());
 			
 			System.out.println("Neue Berechnung durchführen?(y/n)");
 			if(sc.next().charAt(0) == 'y')
 				exec = true;
 			else
 				exec = false;
+			
 		}
+		sc.close();
 	}
 }
